39777972666596245c70670fdfa7116d146e146b
Nataruk <ningchuengtao@gmail.com>
2023-11-03 02:23:05 +0700

    add gif

--- frontend/pages/bingo-player.vue
+++ frontend/pages/bingo-player.vue
@@ -32,6 +32,13 @@
       </v-sheet>
     </v-card>
   </div>
+
+  <img
+    v-if="finish == true"
+    src="/m_gif.gif"
+    style="position: fixed; top: 0; left: 0; width: 100%"
+    alt=""
+  />
 </template>
 <script setup>
 import { ofetch } from "ofetch";
@@ -39,6 +46,7 @@ import { ref } from "vue";
 const name = ref("");
 const nums = ref([]);
 const dialog = ref(true);
+const finish = ref(false);
 function players() {
   nums.value = [];
   let i = 0;
@@ -71,11 +79,41 @@ async function checkNum(n, index) {
       body: { number: n },
     }
   );
-  console.log(check);
 
   if (check.ok == true) {
     nums.value[index].color = "correct";
     new Audio("/green_2.mp3").play();
+    const p = [
+      [0, 1, 2, 3],
+      [4, 5, 6, 7],
+      [8, 9, 10, 11],
+      [12, 13, 14, 15],
+      [0, 4, 8, 12],
+      [1, 5, 9, 13],
+      [2, 6, 10, 14],
+      [3, 7, 11, 15],
+      [0, 5, 10, 15],
+      [3, 6, 9, 12],
+    ];
+
+    for (let i = 0; i < 10; i++) {
+      console.log(
+        p[i],
+        nums.value[p[i][0]],
+        nums.value[p[i][1]],
+        nums.value[p[i][2]],
+        nums.value[p[i][3]]
+      );
+      if (
+        nums.value[p[i][0]].color == "correct" &&
+        nums.value[p[i][3]].color == "correct" &&
+        nums.value[p[i][1]].color == "correct" &&
+        nums.value[p[i][2]].color == "correct"
+      ) {
+        new Audio("/win.mp3").play();
+        finish.value = true;
+      }
+    }
   } else {
     nums.value[index].color = "";
     new Audio("/wrong.mp3").play();
