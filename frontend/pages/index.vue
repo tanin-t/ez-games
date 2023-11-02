e7ab1f60c0cd5951a62fcbf17f91126a6323e1a1
Tanin Thanasopon <t.thanasopon@gmail.com>
2023-11-02 23:14:59 +0700

    Setup home page

---
+++ frontend/pages/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <v-btn href="/millionaire">Millionaire Game</v-btn>
+        <v-btn href="/bingo">Bingo</v-btn>
+        <v-btn href="/matching">Matching Card</v-btn>
+    </div>
+</template>
+
+<script setup>
+</script>
